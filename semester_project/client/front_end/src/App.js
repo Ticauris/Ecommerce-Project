9f485fbb8fb89c04a392176d59fcf095023eac99
Ticauris <ticauris.stokes@usm.edu>
2023-04-19 19:15:28 -0500

    update

--- semester_project/client/front_end/src/App.js
+++ semester_project/client/front_end/src/App.js
@@ -1,26 +1,39 @@
 import React from "react";
 import HomeScreen from "./components/list_products";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductDetailScreen from "./components/ProductDetail";
+import { Navbar, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">fake amazon</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>fake amazon</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/products/:id" element={<ProductDetailScreen />} /> 
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/products/:id" element={<ProductDetailScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
 }
 
 export default App;
-
 
 /* <Route path="/products/:id" element={<ProductDetailScreen />} /> */
